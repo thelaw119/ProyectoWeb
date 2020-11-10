22c32dd89ef03430f46fef2c8e0465b1a2c42335
seiko <>
2020-11-10 15:41:52 -0300

    modificacion de login

--- controlador/Login.php
+++ controlador/Login.php
@@ -10,7 +10,7 @@ session_start();
 
 //$_SESSION['nick'] = $nick;
 
-$SQL = "select Usuarios.nick_usuario, usuarios.clave_usuario, perfiles.codigo_perfil, perfiles.nombre_perfil from Usuarios inner join perfiles on perfiles.codigo_perfil = usuarios.codigo_perfil where nick_usuario = '$nick' and clave_usuario = '$clave'";
+$SQL = "select usuarios.codigo_usuario,usuarios.nick_usuario,usuarios.clave_usuario, usuarios.nombre_usuario,usuarios.apellido_usuario,usuarios.direccion_usuario,usuarios.email_usuario,usuarios.clave_usuario, perfiles.codigo_perfil, perfiles.nombre_perfil from Usuarios inner join perfiles on perfiles.codigo_perfil = usuarios.codigo_perfil where nick_usuario = '$nick' and clave_usuario = '$clave'";
 
 //var_dump($SQL);
 //die();
@@ -20,19 +20,33 @@ $consulta = mysqli_query($conexion, $SQL);
 if($consulta->num_rows > 0){
     //$row = $consulta->fetch_assoc(MYSQLI_ASSOC);
     $row = $consulta->fetch_assoc();
+    $codigo_usuario = $row['codigo_usuario'];
     $nombre_usuario = $row['nick_usuario'];
     $codigo_perfil = $row['codigo_perfil'];
     $nombre_perfil = $row['nombre_perfil'];
+    $nombre_pila = $row['nombre_usuario'];
+    $apellido_usuario = $row['apellido_usuario'];
+    $direccion_usuario = $row['direccion_usuario'];
+    $email_usuario = $row['email_usuario'];
+    $clave_usuario = $row['clave_usuario'];
     
-    var_dump($nombre_usuario,$nombre_perfil);
+//    var_dump($codigo_usuario);
+//    die();
+//    var_dump($nombre_usuario,$nombre_perfil);
     
     if($codigo_perfil== 1){
         $_SESSION['nick_usuario'] = $nombre_usuario;
         $_SESSION['nombre_perfil'] = $nombre_perfil;
         header("Location:http://localhost/ProyectoWeb/vista/Panel_administrador.php");
     }else{
+        $_SESSION['codigo_usuario'] = $codigo_usuario;
         $_SESSION['nick_usuario'] = $nombre_usuario;
         $_SESSION['nombre_perfil'] = $nombre_perfil;
+        $_SESSION['nombre_usuario'] = $nombre_pila;
+        $_SESSION['apellido_usuario'] = $apellido_usuario;
+        $_SESSION['direccion_usuario'] = $direccion_usuario;
+        $_SESSION['email_usuario'] = $email_usuario;
+        $_SESSION['clave_usuario'] = $clave_usuario;
         header("Location:http://localhost/ProyectoWeb/vista/panel.php");
     } 
     
