91ec7102425546d1078fb07f29a15cb4b6887dff
seiko <>
2020-11-17 12:48:33 -0300

    modulo eventos

---
+++ controlador/EditarEvento.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * @autor: seiko
+ */
+session_start();
+require_once '../conexion/Conexion.php';
+
+
+
+  
+$nombre = $_POST["nombre"];
+$descripcion = $_POST["descripcion"];
+$codigo = $_POST["detalle_evento"];
+
+
+
+if ($nombre == '' || $descripcion == '' || $descripcion == '' ) {
+
+echo"<div class='alert alert-warning alert-dismissible'>
+                  <button type=button class='close data-dismiss=alert aria-hidden=true'>&times;</button>
+                  <h5><i class='icon fas fa-info'></i> Advertencia!</h5>
+                  Debe completar los datos.
+                </div>";
+} else {
+$SQL = "UPDATE categorias SET nombre_categoria ='$nombre',descripcion_categoria= '$descripcion' WHERE codigo_categoria = '$codigo'";
+$resultado = mysqli_query($conexion, $SQL);
+
+if ($resultado == true) {
+
+
+
+echo "<div class='alert alert-success alert-dismissible'>
+                  <button type=button class='close data-dismiss=alert aria-hidden=true'>&times;</button>
+                  <h5><i class='icon fas fa-ban'></i> Datos Actualizados Exitosamente</h5>";
+
+
+    } else {
+        echo "<div class = 'alert alert-danger alert-dismissible'>
+<button type = button class = 'close data-dismiss=alert aria-hidden=true'>&times;
+</button>
+<h5><i class = 'icon fas fa-check'></i> Alert!</h5>
+Problemas al actualizar tus datos.
+</div>";
+    }
+}
--- controlador/EliminarEvento.php
+++ controlador/EliminarEvento.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- *   @autor: thelaw
+ *   @autor: seiko
  */
 
 
